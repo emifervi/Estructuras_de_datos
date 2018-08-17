db096c1949dabd2cf40e1a40d8525a3ff9cff4d3
emifervi <emifervi@hotmail.com>
2018-08-16 22:28:27 -0500

    acm prints teams, solved problems, result

--- Tareas/acm.cpp
+++ Tareas/acm.cpp
@@ -2,54 +2,71 @@
 #include <map>
 using namespace std;
 
-struct Equipo{
-    string nombre;
+struct Team{
+    string name;
     map<string, int> problemsSolved;
+    int result;
 
-    Equipo() {
-        nombre = "";
+    Team(){
+        name = "";
+        result = 0;
     }    
 
-    Equipo(string nombre){
-        this->nombre = nombre;
+    Team(string name){
+        this->name = name;
+        this->result = 0;
     }
 };
 
 int main(){
-    int iNumEquipos, iNumProblemas, iNumSoluciones;
+    int numTeams, numProblems, numSolns;
 
-    cin >> iNumEquipos >> iNumProblemas;
+    cin >> numTeams >> numProblems;
 
-    map<string, Equipo> mapaEquipos;
+    map<string, Team> teamMap;
 
-    for (int i = 0; i < iNumEquipos; i++){
-        string sNombre;
-        cin >> sNombre;
+    for (int i = 0; i < numTeams; i++){
+        string teamName;
+        cin >> teamName;
         // Recibe el nombre del equipo, lo pasa como llave y como valor en el mapa
-        mapaEquipos[sNombre] = Equipo(sNombre);
+        teamMap[teamName] = Team(teamName);
     }
 
-    cin >> iNumSoluciones;
+    cin >> numSolns;
     
-    for(int i = 0; i < iNumSoluciones; i++){
+
+    for(int i = 0; i < numSolns; i++){
         // Recibe un nombre de equipo, problema resuelto, tiempo tardado y
         // si fue aceptado o no
-        string sNombre, sProblema, sEstatus;
+        string teamName, problemName, probStatus;
         int iTiempo;
 
-        cin >> sNombre >> sProblema >> iTiempo >> sEstatus;
-
-        Equipo equipo = mapaEquipos[sNombre];
-        if(equipo.problemsSolved.count(sProblema)){
-            equipo.problemsSolved[sProblema] += (sEstatus[0] == 'A') ? iTiempo : 20;
+        cin >> teamName >> problemName >> iTiempo >> probStatus;
+        
+        if(teamMap[teamName].problemsSolved.count(problemName)){
+            teamMap[teamName].problemsSolved[problemName] += (probStatus[0] == 'A') ? iTiempo : 20;
         }
         else{
-            equipo.problemsSolved[sProblema] = (sEstatus[0] == 'A') ? iTiempo : 20;
+            teamMap[teamName].problemsSolved[problemName] = (probStatus[0] == 'A') ? iTiempo : 20;
         }
     }
-    for(auto const& i : mapaEquipos){ //cada equipo
-        cout << i.first << " " << i.second.nombre << endl;
+
+
+    // Itera sobre cada equipo en el mapa imprime el nombre y tamaño
+    for(auto const& i : teamMap){ 
+
+      cout << i.first << " " << teamMap[i.first].problemsSolved.size() << " ";
+      
+      // Itera sobre el mapa de problemas de cada equipo y los suma al resultado
+      // por último imprime el resultado si es mayor a 0 si no imprime un guión
+      for(auto const& j : teamMap[i.first].problemsSolved){
+            teamMap[i.first].result += j.second;
+      }
+      
+      (teamMap[i.first].result > 0) ? cout << teamMap[i.first].result << endl : cout << " -" << endl;
     }
+
+    return 0;
 }
 
 
