ce6dae9e197ff2de63542a29d2bcc55b168dbc31
emifervi <emifervi@hotmail.com>
2018-10-16 21:09:03 -0500

    Modified BST.h and added unit test in main
    *maxWidth returns width of tree
    *mirror inverses the tree vertically
    *copy constructor
    *operator == copmare trees
    *nearestRelative finds nearest relative of 2 ints

--- Clases/BST/main.cpp
+++ Clases/BST/main.cpp
@@ -20,32 +20,52 @@ int main(){
     miArbol.print(3);
 
     /* Purebas tarea programada */
+    // Prueba copyConstructor
+    BST miArbol2(miArbol);
 
     // Prueba LCA
-    cout << "Nearest Relative de: 50 y 65: " << miArbol.nearestRelative(-5, 13) << endl;
+    cout << "Nearest Relative de 50 y 65: " << miArbol.nearestRelative(50, 65) << endl;
+    
+
+    // prueba operator ==
+    if(miArbol == miArbol2){
+        cout << "Los arboles son iguales" << endl;
+    }
+    else{
+        cout << "no fununcio" << endl;
+    }
+
+    // prueba mirror
+    cout << "prueba mirror" << endl;
+    miArbol.mirror();
+
+    // prueba width
+    cout << "prueba width: ";
+    cout << miArbol.maxWidth() << endl;
+    /*
+    // prueba count
+    cout << "El arbol tiene: " << miArbol.count() << " nodos." << endl;
     
     // prueba acestros
     cout << "Ancestros de 65: ";
     miArbol.ancestors(65);
 
-    // prueba count
-    cout << "El arbol tiene: " << miArbol.count() << " nodos." << endl;
-    
     // prueba height
     cout << "Altura del arbol: " << miArbol.height() << endl;
-
-    // prueba print(5)
+    
+    //prueba print(5)
     cout << "Level Order: ";
     miArbol.print(5);
+    miArbol2.print(5);
 
     // prueba whatLevelamI
     cout << "Nivel de 65: ";
     cout << miArbol.whatLevelamI(200) << endl;
-
+    
+    // prueba printleaves recursiva e iterativa
     cout << "Nodos hoja: " << endl;
     miArbol.printLeaves();
-    miArbol.print(4);
-
+    miArbol.print(4); */
     /* Fin pruebas */
 
     if(miArbol.search(100)){
@@ -60,6 +80,7 @@ int main(){
 
     miArbol.remove(12);
     miArbol.print(1);
+    miArbol2.print(1);
     miArbol.print(2);
     miArbol.print(3);
 
